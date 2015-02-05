4e5cd819c6a61938aaf94852908ba333d0a834d6
cloudcome <cloudcome@163.com>
2015-02-05 20:31:13 +0800

    ydr-page

--- index.js
+++ index.js
@@ -10,13 +10,24 @@ var fs = require('fs');
 var dato = require('ydr-util').dato;
 var Pagination = require('./Pagination.js');
 var Template = require('ydr-template');
-var template = fs.readFileSync('./template.html', 'utf8');
-var tpl = new Template(template);
-var defaults = {
-    addClass: '',
+var paginationTemplate = fs.readFileSync('./template/pagination.html', 'utf8');
+var pagerTemplate = fs.readFileSync('./template/pager.html', 'utf8');
+var paginationTpl = new Template(paginationTemplate);
+var pagerTpl = new Template(pagerTemplate);
+var paginationDefaults = {
+    addClass: 'm-pagination',
     max: 1,
     page: 1,
     size: 3
+};
+var pagerDefaults = {
+    addClass: 'm-pager',
+    prev: '上一页',
+    next: '下一页',
+    // 默认为第 1 页
+    page: 1,
+    // 最大页数
+    max: 1
 };
 
 
@@ -25,14 +36,26 @@ var defaults = {
  * @param [options] {Object} 分页数据
  * @returns {String}
  */
-module.exports = function (options) {
-    options = dato.extend(true, {}, defaults, options);
+exports.pagination = function (options) {
+    options = dato.extend(true, {}, paginationDefaults, options);
 
     var list = new Pagination(options);
 
-    return tpl.render(dato.extend(options, {
+    return paginationTpl.render(dato.extend(options, {
         pagination: list
     }));
+};
+
+
+/**
+ * 分页
+ * @param [options] {Object} 分页数据
+ * @returns {String}
+ */
+exports.pager = function (options) {
+    options = dato.extend(true, {}, pagerDefaults, options);
+
+    return pagerTpl.render(options);
 };
 
 
--- test.js
+++ test.js
@@ -6,10 +6,15 @@
 
 'use strict';
 
-var pagination = require('./index.js');
-var html = pagination({
+var page = require('./index.js');
+//var html1 = page.pagination({
+//    max: 100,
+//    page: 33
+//});
+var html2 = page.pager({
     max: 100,
     page: 33
 });
 
-console.log(html);
+//console.log(html1);
+console.log(html2);
